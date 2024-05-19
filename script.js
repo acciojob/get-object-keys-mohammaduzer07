6163449922b942c65ee49e9a96591d9d15c4087a
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-05-19 13:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student =  {
+	name: 'Uzer',
+	age: 23,
+	class: 'BE'
+}
+
+Object.prototype.getKeys = function () {
+	return Object.keys(this);
+}
+
+console.log(student.getKeys());
